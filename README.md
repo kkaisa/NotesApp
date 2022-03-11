29f58d9c5f99ec2b8016b7c2b83acf88c740e4ec
kajoul <kaisajoulu@gmail.com>
2022-03-11 20:28:44 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# NotesApp REST API 
+
+Site link: Coming soon...
+
+#### With a notes app, you can create and store any of your notes for later use.
+
+### Basic Features
+
+#### User should be able to register an account, log in and create, edit, and delete a note.
+
+### The purpose of this project is to learn and practice concepts related to:
+> - Building a REST API
+> - .NET Core
+> - MVC Architectural Pattern
+> - C#
+
+#### More specifically, I used the following:
+> - Dependency injection
+> - JWT
+> - Entity Framework Core O/RM (DBContext, Migration)
+> - Data Transfer Objects (DTOs) & AutoMapper
+> - RESTful API guidelines
+> - HTTP (GET, POST, PUT, PATCH, DELETE, status codes)
+> - Testing API Endpoints (SwaggerUI & Postman)
+> - Docker (Container, Image, Deploying on Docker Hub)
+> - Microsoft Azure (Deployment: Docker Image + Postgres Database)
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,18 +1,87 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using App.DAL.EF;
+using App.Domain.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using WebApp.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using WebApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("NpgsqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//     .AddEntityFrameworkStores<AppDbContext>();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); 
+
+builder.Services
+    .AddAuthentication()
+    .AddCookie(options =>
+        {
+            options.SlidingExpiration = true;
+        }
+    )
+    .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidIssuer = builder.Configuration["JWT:Issuer"],
+                ValidAudience =  builder.Configuration["JWT:Issuer"],
+                            
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes( builder.Configuration["JWT:Key"])),
+                ClockSkew = TimeSpan.Zero
+            };
+        }
+    );
+
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddDefaultUI()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders()
+    ;
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsAllowAll", builder =>
+    {
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+        builder.AllowAnyOrigin();
+    });
+});
+
+// builder.Services.AddAutoMapper(
+//     typeof(DAL.App.Domain.MappingProfiles.AutoMapperProfile),
+//     typeof(BLL.App.DTO.MappingProfiles.AutoMapperProfile)
+// );
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+});
+
+//add supprt for m2m api documentation
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+});
+
+// add support to generate human readable documentation from m2m docs 
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -29,7 +98,17 @@ else
 }
 
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint($"/swagger/v1/swagger.json",
+        "NotesApp"); 
+});
+
 app.UseStaticFiles();
+
+app.UseCors("CorsAllowAll");
 
 app.UseRouting();
 
